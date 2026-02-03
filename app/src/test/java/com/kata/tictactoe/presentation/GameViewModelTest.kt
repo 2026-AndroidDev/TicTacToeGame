ca3b29673f14a36a7326faaef4644f68d784bac1
2026-DEV2-024 <2026-DEV2-024@email.com>
2026-02-03 15:39:10 +0530

    added test case to prevent moves after game finished either won or draw

--- app/src/test/java/com/kata/tictactoe/presentation/GameViewModelTest.kt
+++ app/src/test/java/com/kata/tictactoe/presentation/GameViewModelTest.kt
@@ -40,4 +40,46 @@ class GameViewModelTest {
 
         assertEquals(stateAfterFirstMove, stateAfterInvalidMove)
     }
+
+    @Test
+    fun `when game is won next moves are not allowed or ignored`() = runTest {
+        val viewModel = GameViewModel()
+
+        viewModel.onEvent(UiEvent.OnCellClicked(0)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(3)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(1)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(4)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(2)) // X
+
+        val stateAfterWin = viewModel.state.value
+
+        viewModel.onEvent(UiEvent.OnCellClicked(5))
+
+        val stateAfterExtraMove = viewModel.state.value
+        assertEquals(stateAfterWin, stateAfterExtraMove)
+    }
+
+    @Test
+    fun `when game is draw further moves are ignored`() = runTest {
+        val viewModel = GameViewModel()
+
+        // Fill board with no winner (draw pattern)
+        viewModel.onEvent(UiEvent.OnCellClicked(0)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(1)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(2)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(4)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(3)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(5)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(7)) // X
+        viewModel.onEvent(UiEvent.OnCellClicked(6)) // O
+        viewModel.onEvent(UiEvent.OnCellClicked(8)) // X
+
+        val stateAfterDraw = viewModel.state.value
+
+        // try to play after draw (any index)
+        viewModel.onEvent(UiEvent.OnCellClicked(0))
+
+        val stateAfterExtraMove = viewModel.state.value
+        assertEquals(stateAfterDraw, stateAfterExtraMove)
+    }
 }
